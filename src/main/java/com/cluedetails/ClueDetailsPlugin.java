498f8279ce08e426759ae91565c7da8867d94079
thelope <thelopeosrs@gmail.com>
2025-10-28 19:06:01 -0400

    fix: Limit WorldMapPoint cleanup to instances of ClueDetailsWorldMapPoint

--- src/main/java/com/cluedetails/ClueDetailsPlugin.java
+++ src/main/java/com/cluedetails/ClueDetailsPlugin.java
@@ -605,7 +605,8 @@ public class ClueDetailsPlugin extends Plugin
 		// Remove timers if worldPoint not managed by clueGroundManager
 		clueGroundTimers.removeIf(timer-> !worldPoints.contains(timer.getWorldPoint()));
 		// Remove world map point if worldPoint not managed by clueGroundManager
-		worldMapPointManager.removeIf(worldMapPoint-> !worldPoints.contains(worldMapPoint.getWorldPoint()));
+		worldMapPointManager.removeIf(worldMapPoint->
+			worldMapPoint instanceof ClueDetailsWorldMapPoint && !worldPoints.contains(worldMapPoint.getWorldPoint()));
 
 		// Populate timers
 		for (WorldPoint worldPoint : worldPoints)
